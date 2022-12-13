15afaa28bf19feebdadddffbcc5eb5a2e53113ac
Ivan Braiko <ivan.braiko@jetbrains.com>
2022-12-13 14:55:02 +0300

    fix test

--- tests/test_solve_newton.py
+++ tests/test_solve_newton.py
@@ -14,17 +14,18 @@ def system_two_variables(p):
 
 def system_three_variables(p):
     x, y, z = p
-    return (2*x - z/x, math.exp(y) + x * y, x + y*z + z/2)
+    return (x + y ** 2 - 4 * z, math.exp(x) + x * y - 3, x + y - z)
+
 
 def system_three_variables_2(p):
     x, y, z = p
-    return (x**2+y**2+z**2-1, 2*x**2+y**2-4*z, 3*x**2-4*y+z**2)
+    return (6*x + y ** 2 - 4 * z, z * y - 3, z * y - x)
+
 
 def test_two_variables():
     eps = 10e-6
     (x_expected, y_expected) = fsolve(system_two_variables, (1, 1), epsfcn=eps)
-    x_actual, y_actual = solve_newton(system_two_variables,[1,1] ,eps)
-
+    x_actual, y_actual = solve_newton(system_two_variables, [1, 1], eps)
 
     assert x_expected == pytest.approx(x_actual, eps)
     assert y_expected == pytest.approx(y_actual, eps)
@@ -33,8 +34,7 @@ def test_two_variables():
 def test_three_variables():
     eps = 10e-6
     (x_expected, y_expected, z_expected) = fsolve(system_three_variables, (1, 1, 1), epsfcn=eps)
-    x_actual, y_actual, z_actual = solve_newton(system_three_variables,[1,1,1], eps)
-
+    x_actual, y_actual, z_actual = solve_newton(system_three_variables, [1, 1, 1], eps)
 
     assert x_expected == pytest.approx(x_actual, eps)
     assert y_expected == pytest.approx(y_actual, eps)
@@ -44,7 +44,7 @@ def test_three_variables():
 def test_three_variables_2():
     eps = 0.005
     (x_expected, y_expected, z_expected) = fsolve(system_three_variables_2, (0.5, 0.5, 0.5), epsfcn=eps)
-    x_actual, y_actual, z_actual = solve_newton(system_three_variables_2,[0.5,0.5,0.5], eps)
+    x_actual, y_actual, z_actual = solve_newton(system_three_variables_2, [0.5, 0.5, 0.5], eps)
 
     assert x_expected == pytest.approx(x_actual, eps)
     assert y_expected == pytest.approx(y_actual, eps)
